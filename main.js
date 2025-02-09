cecc7cc7c5295a9b1371642b83557088d1f1a112
Susilo-design <Susilodimas168@gmail.com>
2025-02-10 06:40:07 +0700

    Sunda

---
+++ main.js
@@ -0,0 +1,28 @@
+const menuicon = document.querySelector('#menu-icon')
+const navlink = document.querySelector('.nav-link')
+
+menuicon.onclick = () => {
+    navlink.classList.toggle('active')
+}
+
+
+const toggleButton = document.getElementById('toggleButton');
+const certificateSection = document.getElementById('certificateSection');
+const skillsSection = document.getElementById('skillsSection');
+  
+certificateSection.style.display = 'none';
+
+
+toggleButton.addEventListener('click', () => {
+  if (certificateSection.style.display === 'block') {
+    // Tampilkan Skills
+    certificateSection.style.display = 'none';
+    skillsSection.style.display = 'block';
+    toggleButton.textContent = 'Skills';
+  } else {
+    // Tampilkan Certificates
+    skillsSection.style.display = 'none';
+    certificateSection.style.display = 'block';
+    toggleButton.textContent = 'Certificate';
+  }
+}); 
